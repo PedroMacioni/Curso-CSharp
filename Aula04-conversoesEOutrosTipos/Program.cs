0eccc2c057bd0a138bd9f7594603862e5a2c9d32
Pedro Macioni Tartari <96544140+PedroMacioni@users.noreply.github.com>
2023-03-16 15:44:46 -0300

    Aula 04 - Conversões e outros tipos

---
+++ Aula04-conversoesEOutrosTipos/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("Projeto 4 - Conversões e outros tipos");
+
+        double salario = 3000.15;
+        Console.WriteLine(salario);
+
+        int salarioInteiro = (int)salario;
+        Console.WriteLine(salarioInteiro);
+
+        //Variavel 64bits
+        long x = 2000000000000000;
+        Console.WriteLine(x); 
+
+        //Variavel 16bits
+        short y = 15000;
+        Console.WriteLine(y); 
+
+        //Variavel Decimal
+        float altura = 1.62f;
+        Console.WriteLine(altura);
+
+
+
+        Console.WriteLine("Tecle enter para fechar...");
+        Console.ReadLine();
+    }
+}
