5c594d964b39ceffc0cb8713bc3e5b2154d0682e
Pedro Macioni Tartari <96544140+PedroMacioni@users.noreply.github.com>
2023-03-16 15:46:51 -0300

    Aula 07 - Condicionais

---
+++ Aula07-Condicionais/Program.cs
@@ -0,0 +1,29 @@
+﻿class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Aula 7 - Condicionais");
+
+        int idade = 18;
+        int quantidadePessoas = 3;
+
+        if (idade >= 18)
+        {
+            Console.WriteLine("Você tem mais que 18 anos");
+            Console.WriteLine("Seja bem vindo");
+        }
+        else
+        {
+            if (quantidadePessoas >= 2)
+            {
+                Console.WriteLine("voce nao tem 18, mas pode entrar, pois está acompanhado");
+
+            }
+            else
+            {
+                Console.WriteLine("infelizmente voce nao pode entrar");
+            }
+
+        }
+    }
+}
