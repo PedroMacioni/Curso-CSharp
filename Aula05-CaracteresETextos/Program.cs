d5f5997bd160cf927f008a9fff892d8a9d8b968e
Pedro Macioni Tartari <96544140+PedroMacioni@users.noreply.github.com>
2023-03-16 15:45:44 -0300

    Aula 05 - Caracteres e textos

---
+++ Aula05-CaracteresETextos/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 5 - Caracteres e textos");
+
+        char letra = 'a';
+        Console.WriteLine(letra);
+
+        letra = (char)65;
+        Console.WriteLine(letra);
+
+        letra = (char)(65 + 1);
+        Console.WriteLine(letra);
+
+        letra = (char)(86 + 1);
+        Console.WriteLine(letra);
+
+        string PrimeiraFrase = "Curso de Tecnologia ";
+        Console.WriteLine(PrimeiraFrase + 2022);
+
+        string Vazia = "";
+        Console.WriteLine(Vazia);
+
+        string cursos = @"Cursos disponiveis: 
+- GO 
+- C# 
+- JAVA 
+- PYTHON";
+        Console.WriteLine(cursos);
+;
+        Console.WriteLine("Tecle enter para fechar...") ;
+        Console.ReadLine();
+    }
+}
