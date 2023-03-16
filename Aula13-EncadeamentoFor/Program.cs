125e46cf5e3b434e97350f498fd7bac988cb1974
Pedro Macioni Tartari <96544140+PedroMacioni@users.noreply.github.com>
2023-03-16 15:50:18 -0300

    Aula 13 - Encadeamento for

---
+++ Aula13-EncadeamentoFor/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("Projeto 13 - Encadeamento For");
+
+        //*
+        //**
+        //***
+        //****
+        //*****
+
+        for (int contadorLinhas = 0; contadorLinhas < 10; contadorLinhas++)
+        {
+            for (int contadorColunas = 0; contadorColunas < 10; contadorColunas++)
+            {
+                Console.Write("*");
+                if (contadorColunas >= contadorLinhas)
+                    break;
+            }
+            Console.WriteLine();
+        }
+
+        for (int contadorLinhas = 0; contadorLinhas < 10; contadorLinhas++)
+        {
+            for (int contadorColunas = 0; contadorColunas <= contadorLinhas; contadorColunas++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+
+        Console.WriteLine("Tecle enter para fechar...");
+        Console.ReadLine();
+    }
+}
