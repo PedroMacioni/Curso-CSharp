fa58979f969f6aa3a060aa771f1486a7e8348ddf
Pedro Macioni Tartari <96544140+PedroMacioni@users.noreply.github.com>
2023-03-16 15:47:25 -0300

    Aula 08 - Segunda parte Condicionais

---
+++ Aula08-Condicionais02/Program.cs
@@ -0,0 +1,25 @@
+﻿class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 8 - Condicionais 2");
+
+        int idadeJoao = 16;
+        int quantidadePessoas = 2;
+
+        bool acompanhado = quantidadePessoas > 1;
+        bool grupo = true;
+
+        if (idadeJoao >= 18 || acompanhado)
+        {
+            Console.WriteLine("Pode entrar");
+        }
+        else
+        {
+            Console.WriteLine("Não pode entrar");
+        }
+
+        Console.WriteLine("Tecle enter para fechar");
+        Console.ReadLine();
+    }
+}
