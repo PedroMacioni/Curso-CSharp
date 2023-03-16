e3929260867d2f8dc1919a66777167464ebc4d18
Pedro Macioni Tartari <96544140+PedroMacioni@users.noreply.github.com>
2023-03-16 15:46:18 -0300

    Aula 06 - Atribuição de variáveis

---
+++ Aula06-AtribuicoesDeVariaveis/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("Projeto 6 - Atribuições De Variáveis");
+
+        int idade = 30;
+        int idadeAna = idade;
+
+        Console.WriteLine(idadeAna);
+
+        idade = 25;
+
+        Console.WriteLine("Tecle enter para fechar...");
+        Console.ReadLine();
+    }
+}
