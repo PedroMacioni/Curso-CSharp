af3c4dcc2d5a8ecf93e431ec981939024a633426
Pedro Macioni Tartari <96544140+PedroMacioni@users.noreply.github.com>
2023-03-16 15:35:04 -0300

    Aula 02 - Criando Variáveis

---
+++ Aula02-CriandoVariaveis/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 2 - Criando Variaveis");
+
+        int idade;
+        
+        idade = 27;
+
+        Console.WriteLine("Minha idade é " + idade);
+
+        idade = 27 - 5;
+
+        Console.WriteLine(idade);
+
+        idade = 5 * 2 - 6;
+
+        Console.WriteLine(idade);
+
+        idade = (5 - 2) * 3;
+
+        Console.WriteLine(idade);
+
+        Console.WriteLine("Tecle enter para fechar ...");
+        Console.ReadLine();
+    }
+}
