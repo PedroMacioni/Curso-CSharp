2c1b80be25438e34fecacf7dcc19f5438f1084dc
Pedro Macioni Tartari <96544140+PedroMacioni@users.noreply.github.com>
2023-03-16 15:49:53 -0300

    Aula 12 - Investimento a longo prazo

---
+++ Aula12-InvestimentoALongoPrazo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO.Pipes;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("Projeto 12 - Investindo a longo prazo");
+
+        double FatorRendimento = 1.005;
+        double investimento = 1000;
+
+        for (int anos = 1; anos <= 5; anos++)
+        {
+            for (int mes = 1; mes <= 12; mes++)
+            {
+                investimento *= FatorRendimento + 0.001;
+            }
+
+            FatorRendimento = FatorRendimento + 0.001;
+        }
+
+        Console.WriteLine("Depois de 5 anos você terá R$ " + investimento);
+
+        Console.WriteLine("Tecle enter para fechar...");
+        Console.ReadLine();
+    }
+}
