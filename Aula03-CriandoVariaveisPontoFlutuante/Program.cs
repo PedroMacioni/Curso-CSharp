7f2f50e4d15a3fdc03414d2ee804c6d3b6c97f5e
Pedro Macioni Tartari <96544140+PedroMacioni@users.noreply.github.com>
2023-03-16 15:43:52 -0300

    Aula 03 - Criando variáveis ponto flutuante

---
+++ Aula03-CriandoVariaveisPontoFlutuante/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 3 - Variáveis Ponto Flutuante");
+
+        double salario;
+        salario = 3000.10;
+
+        salario = 3000;
+
+        Console.WriteLine(salario);
+
+        Console.WriteLine("Tecle enter para fechar...");
+        Console.ReadLine();
+    }
+}
