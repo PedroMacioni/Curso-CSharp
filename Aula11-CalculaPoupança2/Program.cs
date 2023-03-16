ad0c1672d3686408b76f28d8e009803d44c09276
Pedro Macioni Tartari <96544140+PedroMacioni@users.noreply.github.com>
2023-03-16 15:49:27 -0300

    Aula 11 - Segunda parte calcula poupança

---
+++ Aula11-CalculaPoupança2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("Projeto 10 - Calculo Poupança");
+
+        /*
+        while (mes <= 12)
+        {
+            investimento = investimento + investimento * 0.005;
+            Console.WriteLine("No mês " + mes + " você tem R$" + investimento);
+
+            //mes = mes + 1;
+            mes += 1;
+        }
+        */
+
+        double investimento = 1000;
+
+        for (int mes = 1; mes <= 12; mes = mes + 1)
+        {
+            investimento = investimento *= 1.005;
+            Console.WriteLine("No mês " + mes + " você tem R$" + investimento);
+        }
+
+        Console.WriteLine("Tecle enter para fechar...");
+        Console.ReadLine();
+    }
+}
